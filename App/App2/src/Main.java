0713f57ed7979b7e712c5a2863fa31dd342345b4
tadepalliruthvika <tadepalliruthvika@gmail.com>
2026-03-20 09:22:50 +0530

    uc2commited

---
+++ App/App2/src/Main.java
@@ -0,0 +1,56 @@
+abstract class Room {
+    String type;
+    int beds;
+    double price;
+
+    Room(String type, int beds, double price) {
+        this.type = type;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    void display() {
+        System.out.println(type + " | Beds: " + beds + " | Price: ₹" + price);
+    }
+}
+
+class SingleRoom extends Room {
+    SingleRoom() {
+        super("Single Room", 1, 1000);
+    }
+}
+
+class DoubleRoom extends Room {
+    DoubleRoom() {
+        super("Double Room", 2, 1800);
+    }
+}
+
+class SuiteRoom extends Room {
+    SuiteRoom() {
+        super("Suite Room", 3, 3000);
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Room r1 = new SingleRoom();
+        Room r2 = new DoubleRoom();
+        Room r3 = new SuiteRoom();
+
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        r1.display();
+        System.out.println("Available: " + singleAvailable);
+
+        r2.display();
+        System.out.println("Available: " + doubleAvailable);
+
+        r3.display();
+        System.out.println("Available: " + suiteAvailable);
+    }
+}
